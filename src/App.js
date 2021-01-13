91705b2a23d844edc1cfcd8f78f4bb748c55e9fa
Koodikessi <hessuhomo1@hotmail.com>
2021-01-13 11:31:36 +0200

    coupon claim added

--- src/App.js
+++ src/App.js
@@ -7,19 +7,37 @@ import Coupons from "./Coupons";
 import Profile from "./Profile";
 
 import "./App.css";
+import allCoupons from "./allCoupons";
 
 class Clicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
       clicks: 0,
+      coupons: [],
     };
     this.setClicks = this.setClicks.bind(this);
+    this.claimCoupon = this.claimCoupon.bind(this);
   }
 
   setClicks(clicks) {
     this.setState({
       clicks: clicks,
+    });
+  }
+
+  claimCoupon(couponID) {
+    let filteredCoupons = allCoupons.filter((offer) => offer.id === couponID);
+    let selectedCoupon = Object.assign({}, filteredCoupons[0]);
+    selectedCoupon.claimed = Date.now();
+    selectedCoupon.validDue = Date.now() + 14 * 24 * 60 * 60 * 1000;
+    let clicks = this.state.clicks;
+    clicks = clicks - selectedCoupon.price;
+    let coupons = this.state.coupons.slice();
+    coupons.push(selectedCoupon);
+    this.setState({
+      clicks: clicks,
+      coupons: coupons,
     });
   }
 
@@ -36,7 +54,12 @@ class Clicker extends Component {
           />
           <Route
             path="/coupon"
-            render={(props) => <Coupons clicks={this.state.clicks} />}
+            render={(props) => (
+              <Coupons
+                clicks={this.state.clicks}
+                claimCoupon={this.claimCoupon}
+              />
+            )}
           />
           <Route path="/profile" component={Profile} />
           <Menu claimableCoupons={7} />
